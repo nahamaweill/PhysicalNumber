298e9c49331d4a96c110f0bb52f94a934959d857
maayanbuzaglo <maayan13500@gmail.com>
2019-04-14 19:28:46 +0300

    Almost added all the functions

--- PhysicalNumber.cpp
+++ PhysicalNumber.cpp
@@ -50,134 +50,131 @@ bool PhysicalNumber::sameGroup(PhysicalNumber& num1, PhysicalNumber& num2)
 
 void PhysicalNumber::convertIfSameGroup(PhysicalNumber& num1, PhysicalNumber& num2)
 {
-
-    // Check if numbers are in the Length group.
+    //Checks if the numbers are in the size group.
     if((num1.unit == M) || (num1.unit == CM) || (num1.unit == KM)) 
     {
-        if (((num1.unit == KM) && (num2.unit == M)) || ((num1.unit == M) && (num2.unit == KM))) // Checking if the numbers are from type 'km' and 'm'.
+        if (((num1.unit == KM) && (num2.unit == M)) || ((num1.unit == M) && (num2.unit == KM))) //Checks if the numbers are from type 'km' and 'm'.
         {
-            if (num1.unit == KM) // Check if num1 is the bigger number.
+            if (num1.unit == KM) //Checks if num1 is the bigger number.
             {
-                num2.setData(num2.data/1000);
+                num2.setData(num2.data / 1000);
                 num2.setUnit(KM);
             }   
-            else // Else num2 is the bigger number.
+            else //Else - num2 is the bigger number.
             {
-                num1.setData(num1.data/1000);
+                num1.setData(num1.data / 1000);
                 num1.setUnit(KM);
             }
         }
-        else if (((num1.unit == KM) && (num2.unit == CM)) || ((num1.unit == CM) && (num2.unit == KM)))// Checking if the numbers are from type 'km' and 'cm'.
+        else if (((num1.unit == KM) && (num2.unit == CM)) || ((num1.unit == CM) && (num2.unit == KM))) //Checks if the numbers are from type 'km' and 'cm'.
         {
-            if (num1.unit == KM) // Check if num1 is the bigger number.
+            if (num1.unit == KM) //Checks if num1 is the bigger number.
             {
-                num2.setData(num2.data/100000);
+                num2.setData(num2.data / 100000);
                 num2.setUnit(KM);
-
             }   
-            else // Else num2 is the bigger number.
+            else //Else - num2 is the bigger number.
             {
-                num1.setData(num1.data/100000);
+                num1.setData(num1.data / 100000);
                 num1.setUnit(KM);
-
             }
         }
-        else if (((num1.unit == M) && (num2.unit == CM)) || ((num1.unit == CM) && (num2.unit == M)))// Checking if the numbers are from type 'm' and 'cm'.
+        else if (((num1.unit == M) && (num2.unit == CM)) || ((num1.unit == CM) && (num2.unit == M))) //Checks if the numbers are from type 'm' and 'cm'.
         {
-            if (num1.unit == M) // Check if num1 is the bigger number.
+            if (num1.unit == M) //Checks if num1 is the bigger number.
             {
-                num2.setData(num2.data/100);
+                num2.setData(num2.data / 100);
                 num2.setUnit(M);
             }   
-            else // Else num2 is the bigger number.
+            else // Else - num2 is the bigger number.
             {
-                num1.setData(num1.data/100);
+                num1.setData(num1.data / 100);
                 num1.setUnit(M);
             }
         }
     }
-    // Check if the numbers are in the Time group.
+    //Checks if the numbers are in the time group.
     else if ((num1.unit == HOUR) || (num1.unit == MIN) || (num1.data == SEC)) 
     {
-        if (((num1.unit == HOUR) && (num2.unit == MIN)) || ((num1.unit == MIN) && (num2.unit == HOUR))) // Checking if the numbers are from type 'hour' and 'min'.
+        if (((num1.unit == HOUR) && (num2.unit == MIN)) || ((num1.unit == MIN) && (num2.unit == HOUR))) //Checks if the numbers are from type 'hour' and 'min'.
         {
-            if (num1.unit == HOUR) // Check if num1 is the bigger number.
+            if (num1.unit == HOUR) //Checks if num1 is the bigger number.
             {
-                num2.setData(num2.data/60);
+                num2.setData(num2.data / 60);
                 num2.setUnit(HOUR);
             }   
-            else // Else num2 is the bigger number.
+            else //Else - num2 is the bigger number.
             {
-                num1.setData(num1.data/60);
+                num1.setData(num1.data / 60);
                 num1.setUnit(HOUR);
             }
         }
-        else if (((num1.unit == HOUR) && (num2.unit == SEC)) || ((num1.unit == SEC) && (num2.unit == HOUR))) // Checking if the numbers are from type 'hour' and 'sec'.
+        else if (((num1.unit == HOUR) && (num2.unit == SEC)) || ((num1.unit == SEC) && (num2.unit == HOUR))) //Checks if the numbers are from type 'hour' and 'sec'.
         {
-            if (num1.unit == HOUR) // Check if num1 is the bigger number.
+            if (num1.unit == HOUR) //Checks if num1 is the bigger number.
             {
-                num2.setData(num2.data/3600);
+                num2.setData(num2.data / 3600);
                 num2.setUnit(HOUR);
             }   
-            else // Else num2 is the bigger number.
+            else //Else - num2 is the bigger number.
             {
-                num1.setData(num1.data/3600);
+                num1.setData(num1.data / 3600);
                 num1.setUnit(HOUR);
             }
         }
-        else if (((num1.unit == MIN) && (num2.unit == SEC)) || ((num1.unit == SEC) && (num2.unit == MIN))) // Checking if the numbers are from type 'min' and 'sec'.
+        else if (((num1.unit == MIN) && (num2.unit == SEC)) || ((num1.unit == SEC) && (num2.unit == MIN))) //Checks if the numbers are from type 'min' and 'sec'.
         {
-            if (num1.unit == MIN) // Check if num1 is the bigger number.
+            if (num1.unit == MIN) //Checks if num1 is the bigger number.
             {
-                num2.setData(num2.data/60);
+                num2.setData(num2.data / 60);
                 num2.setUnit(MIN);
             }   
-            else // Else num2 is the bigger number.
+            else //Else - num2 is the bigger number.
             {
-                num1.setData(num1.data/60);
+                num1.setData(num1.data / 60);
                 num1.setUnit(MIN);
             }
         }
     }
-    // Check if the numbers are in the weight group.
+    //Checks if the numbers are in the weight group.
     else if ((num1.unit == TON) || (num1.unit == KG) || (num1.data == G)) 
     {
-        if (((num1.unit == TON) && (num2.unit == KG)) || ((num1.unit == KG) && (num2.unit == TON))) // Checking if the numbers are from type 'ton' and 'kg'.
+        if (((num1.unit == TON) && (num2.unit == KG)) || ((num1.unit == KG) && (num2.unit == TON))) //Checks if the numbers are from type 'ton' and 'kg'.
         {
-            if (num1.unit == TON) // Check if num1 is the bigger number.
+            if (num1.unit == TON) //Checks if num1 is the bigger number.
             {
-                num2.setData(num2.data/1000);
+                num2.setData(num2.data / 1000);
                 num2.setUnit(TON);
             }   
-            else // Else num2 is the bigger number.
+            else //Else - num2 is the bigger number.
             {
-                num1.setData(num1.data/1000);
+                num1.setData(num1.data / 1000);
                 num1.setUnit(TON);
             }
         }
-        else if (((num1.unit == TON) && (num2.unit == G)) || ((num1.unit == G) && (num2.unit == TON))) // Checking if the numbers are from type 'ton' and 'g'.
+        else if (((num1.unit == TON) && (num2.unit == G)) || ((num1.unit == G) && (num2.unit == TON))) //Checks if the numbers are from type 'ton' and 'g'.
         {
-            if (num1.unit == TON) // Check if num1 is the bigger number.
+            if (num1.unit == TON) //Checks if num1 is the bigger number.
             {
-                num2.setData(num2.data/1000000);
+                num2.setData(num2.data / 1000000);
                 num2.setUnit(TON);
             }   
-            else // Else num2 is the bigger number.
+            else //Else - num2 is the bigger number.
             {
-                num1.setData(num1.data/1000000);
+                num1.setData(num1.data / 1000000);
                 num1.setUnit(TON);
             }
         }
-        else if (((num1.unit == KG) && (num2.unit == G)) || ((num1.unit == G) && (num2.unit == KG))) // Checking if the numbers are from type 'kg' and 'g'.
+        else if (((num1.unit == KG) && (num2.unit == G)) || ((num1.unit == G) && (num2.unit == KG))) //Checks if the numbers are from type 'kg' and 'g'.
         {
-            if (num1.unit == KG) // Check if num1 is the bigger number.
+            if (num1.unit == KG) //Checks if num1 is the bigger number.
             {
-                num2.setData(num2.data/1000);
+                num2.setData(num2.data / 1000);
                 num2.setUnit(KG);
             }   
-            else // Else num2 is the bigger number.
+            else //Else - num2 is the bigger number.
             {
-                num1.setData(num1.data/1000);
+                num1.setData(num1.data / 1000);
                 num1.setUnit(KG);
             }
         }
@@ -191,32 +188,44 @@ and convertIfSameGroup function.
 */
 PhysicalNumber PhysicalNumber::operator+(PhysicalNumber& other)
 {
+    PhysicalNumber tempThis = PhysicalNumber(this->data, this->unit); //temp for this.
+    PhysicalNumber tempOther = PhysicalNumber(other.data, other.unit); //temp for other.
+
     //If they are not on the same group.
-    if(sameGroup(*this, other) == false)
+    if(sameGroup(tempThis, tempOther) == false)
     {
         __throw_runtime_error("They are not on the same group.");
     }
     //If they have the same unit.
-    else if(this->unit == other.unit)
+    else if(tempThis.unit == tempOther.unit)
     {
-        return PhysicalNumber(this->data + other.data, this->unit);
+        return PhysicalNumber(tempThis.data + tempOther.data, tempThis.unit);
     }
     //If they don't have the same unit but they are on the same group.
     else
     {
-        convertIfSameGroup(*this, other);
+        convertIfSameGroup(tempThis, tempOther);
     }
-    return PhysicalNumber(this->data + other.data, this->unit);
+    return PhysicalNumber(tempThis.data + tempOther.data, tempThis.unit);
 }
 
-////////////////////////////////////////////////
+/*
+This function changes the data of the unit to the it's positive number.
+*/
 const PhysicalNumber PhysicalNumber::operator+()
 {
-    return PhysicalNumber(0, Unit::M);
+    return *this;
 }
 
+/*
+This function sums 2 PhysicalNumbers if they on the same units group by convert the smaller to the bigger,
+and put it in the first number.
+It gets help from operator+ function.
+*/
 PhysicalNumber& PhysicalNumber::operator+=(const PhysicalNumber& other)
 {
+    PhysicalNumber tempOther(other.data, other.unit);  //temp PhysicalNumber.
+    *this = (*this).operator+(tempOther);
     return *this;
 }
 
@@ -227,77 +236,257 @@ and convertIfSameGroup function.
 */
 PhysicalNumber PhysicalNumber::operator-(PhysicalNumber& other)
 {
+    PhysicalNumber tempThis = PhysicalNumber(this->data, this->unit);
+    PhysicalNumber tempOther = PhysicalNumber(other.data, other.unit);
+
     //If they are not on the same group.
-    if(sameGroup(*this, other) == false)
+    if(sameGroup(tempThis, tempOther) == false)
+    {
+        __throw_runtime_error("They are not on the same group.");
+    }
+    //If they have the same unit.
+    else if(tempThis.unit == tempOther.unit)
+    {
+        return PhysicalNumber(tempThis.data - tempOther.data, tempThis.unit);
+    }
+    //If they don't have the same unit but they are on the same group.
+    else
+    {
+        convertIfSameGroup(tempThis, tempOther);
+    }
+    return PhysicalNumber(tempThis.data - tempOther.data, tempThis.unit);
+}
+
+/*
+This function changes the data of the unit to the it's negative number.
+*/
+const PhysicalNumber PhysicalNumber::operator-()
+{
+    this->setData((this->data) * (-1));
+    return *this;
+}
+
+/*
+This function subtracts 2 PhysicalNumbers if they on the same units group by convert the smaller to the bigger,
+and put it in the first number.
+It gets help from operator- function.
+*/
+PhysicalNumber& PhysicalNumber::operator-=(const PhysicalNumber& other)
+{
+    PhysicalNumber tempOther(other.data, other.unit); //temp PhysicalNumber.
+    *this = (*this).operator-(tempOther);
+    return *this;
+}
+
+/*
+This function adds 1 to the data.
+*/
+PhysicalNumber& PhysicalNumber::operator++()
+{
+    this->setData(this->data + 1);
+    return *this;
+}
+
+/*
+This function subtracts 1 to the data.
+*/
+PhysicalNumber& PhysicalNumber::operator--()
+{
+    if(this->data < 1) //Checks if the data is smaller than 1.
+    {
+        __throw_runtime_error("The number is smaller than 1.");
+        exit;
+    }
+
+    this->setData(this->data - 1);
+    return *this;
+}
+
+const bool PhysicalNumber::operator>(const PhysicalNumber& other)
+{
+    PhysicalNumber tempThis(this->data, this->unit);
+    PhysicalNumber tempOther(other.data, other.unit);
+
+    //If they are not on the same group.
+    if(sameGroup(tempThis, tempOther) == false)
     {
         __throw_runtime_error("They are not on the same group.");
     }
     //If they have the same unit.
     else if(this->unit == other.unit)
     {
-        return PhysicalNumber(this->data - other.data, this->unit);
+        return (this->data > other.data);
     }
     //If they don't have the same unit but they are on the same group.
     else
     {
-        convertIfSameGroup(*this, other);
+        convertIfSameGroup(tempThis, tempOther);
     }
-    return PhysicalNumber(this->data - other.data, this->unit);
-}
-
-const PhysicalNumber PhysicalNumber::operator-()
-{
-    return PhysicalNumber(0, Unit::M);
-}
-
-PhysicalNumber& PhysicalNumber::operator-=(const PhysicalNumber& other)
-{
-    return *this;
-}
-
-PhysicalNumber& PhysicalNumber::operator++()
-{
-    return *this;
-}
-
-PhysicalNumber& PhysicalNumber::operator--()
-{
-    return *this;
-}
-
-const bool PhysicalNumber::operator>(const PhysicalNumber& other)
-{
-    return false;
+    return (tempThis.data > tempOther.data);
+    
 }
 
 const bool PhysicalNumber::operator<(const PhysicalNumber& other)
 {
-    return false;
+    PhysicalNumber tempThis(this->data, this->unit);
+    PhysicalNumber tempOther(other.data, other.unit);
+
+    //If they are not on the same group.
+    if(sameGroup(tempThis, tempOther) == false)
+    {
+        __throw_runtime_error("They are not on the same group.");
+    }
+    //If they have the same unit.
+    else if(this->unit == other.unit)
+    {
+        return (this->data < other.data);
+    }
+    //If they don't have the same unit but they are on the same group.
+    else
+    {
+        convertIfSameGroup(tempThis, tempOther);
+    }
+    return (tempThis.data < tempOther.data);
 }
 
 const bool PhysicalNumber::operator<=(const PhysicalNumber& other)
 {
-    return false;
+    PhysicalNumber tempThis(this->data, this->unit);
+    PhysicalNumber tempOther(other.data, other.unit);
+
+    //If they are not on the same group.
+    if(sameGroup(tempThis, tempOther) == false)
+    {
+        __throw_runtime_error("They are not on the same group.");
+    }
+    //If they have the same unit.
+    else if(this->unit == other.unit)
+    {
+        return (this->data <= other.data);
+    }
+    //If they don't have the same unit but they are on the same group.
+    else
+    {
+        convertIfSameGroup(tempThis, tempOther);
+    }
+    return (tempThis.data <= tempOther.data);
 }
 
 const bool PhysicalNumber::operator>=(const PhysicalNumber& other)
 {
-    return false;
+    PhysicalNumber tempThis(this->data, this->unit);
+    PhysicalNumber tempOther(other.data, other.unit);
+
+    //If they are not on the same group.
+    if(sameGroup(tempThis, tempOther) == false)
+    {
+        __throw_runtime_error("They are not on the same group.");
+    }
+    //If they have the same unit.
+    else if(this->unit == other.unit)
+    {
+        return (this->data >= other.data);
+    }
+    //If they don't have the same unit but they are on the same group.
+    else
+    {
+        convertIfSameGroup(tempThis, tempOther);
+    }
+    return (tempThis.data >= tempOther.data);
 }
 
 const bool PhysicalNumber::operator==(const PhysicalNumber& other)
 {
-    return false;
+    PhysicalNumber tempThis(this->data, this->unit);
+    PhysicalNumber tempOther(other.data, other.unit);
+
+    //If they are not on the same group.
+    if(sameGroup(tempThis, tempOther) == false)
+    {
+        __throw_runtime_error("They are not on the same group.");
+    }
+    //If they have the same unit.
+    else if(this->unit == other.unit)
+    {
+        return (this->data == other.data);
+    }
+    //If they don't have the same unit but they are on the same group.
+    else
+    {
+        convertIfSameGroup(tempThis, tempOther);
+    }
+    return (tempThis.data == tempOther.data);
 }
 
 const bool PhysicalNumber::operator!=(const PhysicalNumber& other)
 {
-    return false;
+    PhysicalNumber tempThis(this->data, this->unit);
+    PhysicalNumber tempOther(other.data, other.unit);
+
+    //If they are not on the same group.
+    if(sameGroup(tempThis, tempOther) == false)
+    {
+        return true;
+    }
+    //If they have the same unit.
+    else if(this->unit == other.unit)
+    {
+        return (this->data != other.data);
+    }
+    //If they don't have the same unit but they are on the same group.
+    else
+    {
+        convertIfSameGroup(tempThis, tempOther);
+    }
+    return (tempThis.data != tempOther.data);
 }
 
 ostream& ariel::operator<<(ostream& os, const PhysicalNumber& pn)
 {
-    return os << "";
+    string type = " ";
+    switch(pn.unit)
+    {
+        case KM:
+        type = "km";
+        break;
+
+        case M:
+        type = "m";
+        break;
+
+        case CM:
+        type = "cm";
+        break;
+
+        case HOUR:
+        type = "hour";
+        break;
+
+        case MIN:
+        type = "min";
+        break;
+
+        case SEC:
+        type = "sec";
+        break;
+
+        case TON:
+        type = "ton";
+        break;
+
+        case KG:
+        type = "kg";
+        break;
+
+        case G:
+        type = "g";
+        break;
+
+        default:
+        cout << "ERROR";
+    }
+
+    return (os << pn.data << '[' << type << ']') ;
 }
 
 istream& ariel::operator>>(istream& is, PhysicalNumber& pn)
